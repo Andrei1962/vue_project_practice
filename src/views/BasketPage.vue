043c15ccdeef498abf0b197cb189e6cdaf96f19d
Andrei Stolarov <andrey.stolyarov.1962@mail.ru>
2025-03-19 20:06:42 +0300

    устранил замея

---
+++ src/views/BasketPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <BaseActionIcon/>
+    <h1 class="bufer">BasketPage</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BaseActionIcon from '@/components/icons/BaseActionIcon.vue'
+
+export default {
+  name: 'BasketPage',
+  components: {
+    BaseActionIcon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  widht: 1440px;
+  height: 1379px;
+  background-color: #161516;
+
+  .bufer {
+  color: #ffffff;
+  }
+}
+
+</style>
